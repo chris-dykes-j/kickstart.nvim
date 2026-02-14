b4a24d3bfeca8de1b8579fff342a8066cfcfbcd9
Christopher Dykes <chris.t.c.dykes@gmail.com>
2026-02-14 10:29:41 -0500

    Fix tab size. Remove harpoon for now (strange bug).

--- init.lua
+++ init.lua
@@ -147,8 +147,8 @@ vim.o.splitbelow = true
 --  It is very similar to `vim.o` but offers an interface for conveniently interacting with tables.
 --   See `:help lua-options`
 --   and `:help lua-guide-options`
-vim.o.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+--vim.o.list = true
+--vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
 -- Preview substitutions live, as you type!
 vim.o.inccommand = 'split'
@@ -657,12 +657,13 @@ require('lazy').setup({
     end,
   },
 
-  -- Harpoon plugin
+  --[[ Harpoon plugin
   {
     'ThePrimeagen/harpoon',
     branch = 'harpoon2',
     dependencies = { 'nvim-lua/plenary.nvim' },
   },
+  --]]
 
   { -- Autoformat
     'stevearc/conform.nvim',
@@ -945,25 +946,9 @@ require('lazy').setup({
   },
 })
 
--- Harpoon set up
-
-local harpoon = require 'harpoon'
-
--- REQUIRED
-harpoon:setup()
--- REQUIRED
-
-vim.keymap.set('n', '<leader>a', function() harpoon:list():add() end)
-vim.keymap.set('n', '<C-e>', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-
-vim.keymap.set('n', '<C-h>', function() harpoon:list():select(1) end)
-vim.keymap.set('n', '<C-t>', function() harpoon:list():select(2) end)
-vim.keymap.set('n', '<C-n>', function() harpoon:list():select(3) end)
-vim.keymap.set('n', '<C-s>', function() harpoon:list():select(4) end)
-
--- Toggle previous & next buffers stored within Harpoon list
-vim.keymap.set('n', '<C-S-P>', function() harpoon:list():prev() end)
-vim.keymap.set('n', '<C-S-N>', function() harpoon:list():next() end)
-
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+
+-- Tabs
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
