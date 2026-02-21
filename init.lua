f7e6ca5a363164cf6952e75ed33b12dda095230f
Christopher Dykes <chris.t.c.dykes@gmail.com>
2026-02-21 16:47:01 -0500

    Limit prose plugin to md and txt files.

--- init.lua
+++ init.lua
@@ -985,11 +985,18 @@ vim.keymap.set('n', '<C-n>', function() harpoon:list():select(3) end)
 vim.keymap.set('n', '<C-s>', function() harpoon:list():select(4) end)
 
 -- Prose Plugin Setup
-local prose = require 'prose'
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'markdown', 'text' },
+  callback = function()
+    local prose = require 'prose'
 
-vim.keymap.set('n', '<C-q>', function() prose.quote:add_quotations() end, { desc = 'Add quotation marks to current line' })
-vim.keymap.set('i', '<C-q>', function() prose.quote:add_quotations() end, { desc = 'Add quotation marks to current line' })
-vim.keymap.set('x', 'q', function() prose.quote:add_quotes_to_selection() end, { desc = 'Add quotation marks to the current selection line by line' })
+    -- Quotations
+    vim.keymap.set('n', '<C-q>', function() prose.quote:add_quotations() end, { desc = 'Add quotation marks to current line' })
+    vim.keymap.set('i', '<C-q>', function() prose.quote:add_quotations() end, { desc = 'Add quotation marks to current line' })
+    vim.keymap.set('x', 'q', function() prose.quote:add_quotes_to_selection() end, { desc = 'Add quotation marks to the current selection line by line' })
 
-vim.keymap.set('n', '<C-i>', function() prose.italicize:italicize() end, { desc = 'Italicize current line' })
-vim.keymap.set('i', '<C-i>', function() prose.italicize:italicize() end, { desc = 'Italicize current line' })
+    -- Italics
+    vim.keymap.set('n', '<C-i>', function() prose.italicize:italicize() end, { desc = 'Italicize current line' })
+    vim.keymap.set('i', '<C-i>', function() prose.italicize:italicize() end, { desc = 'Italicize current line' })
+  end,
+})
